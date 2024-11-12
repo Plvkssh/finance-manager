ffa7e0ccc376e003335bf688316531defec53029
Plvkssh <kksmartregion@gmail.com>
2024-11-12 11:58:28 +0700

    Update ChessPiece.java

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,1 +1,15 @@
+// Класс ChessPiece должен быть абстрактным и от него должны наследоваться все фигуры
 
+public abstract class ChessPiece {
+    private String color;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract boolean canMoveToPosition(ChessBoard board, int startRow, int startCol, int endRow, int endCol);
+}
