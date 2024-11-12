6008103f54e17591050e10e546730c346ce8628a
Plvkssh <kksmartregion@gmail.com>
2024-11-12 12:10:10 +0700

    Update King.java

--- King.java
+++ King.java
@@ -1,1 +1,17 @@
+public class King extends ChessPiece {
+    public King(String color) {
+        super(color);
+    }
 
+    @Override
+    public boolean canMoveToPosition(ChessBoard board, int startRow, int startCol, int endRow, int endCol) {
+        if (!isValidPosition(endRow, endCol)) return false;
+        int rowDiff = Math.abs(startRow - endRow);
+        int colDiff = Math.abs(startCol - endCol);
+        return (rowDiff <= 1 && colDiff <= 1) && (board.board[endRow][endCol] == null || !board.board[endRow][endCol].getColor().equals(this.getColor()));
+    }
+
+    private boolean isValidPosition(int row, int col) {
+        return row >= 0 && row < 8 && col >= 0 && col < 8;
+    }
+}
