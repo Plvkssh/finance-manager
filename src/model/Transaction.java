c9e705410e324d467b5f2db5b0b41a10b5989cb2
Plvkssh <kksmartregion@gmail.com>
2025-01-18 19:54:22 +0700

    Update Transaction.java

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,27 +1,30 @@
 package model;
 
-import java.io.Serializable;
+// Класс Transaction представляет транзакцию: доход или расход
+public class Transaction {
+    private String type; // Тип транзакции: "income" или "expense"
+    private double amount; // Сумма транзакции
+    private String category; // Категория транзакции
 
-public class Transaction implements Serializable {
-    private final String category;
-    private final double amount;
-    private final boolean isIncome;
-
-    public Transaction(String category, double amount, boolean isIncome) {
-        this.category = category;
+    // Конструктор для инициализации транзакции
+    public Transaction(String type, double amount, String category) {
+        this.type = type;
         this.amount = amount;
-        this.isIncome = isIncome;
+        this.category = category;
     }
 
-    public String getCategory() {
-        return category;
+    // Геттер для получения типа транзакции
+    public String getType() {
+        return type;
     }
 
+    // Геттер для получения суммы транзакции
     public double getAmount() {
         return amount;
     }
 
-    public boolean isIncome() {
-        return isIncome;
+    // Геттер для получения категории транзакции
+    public String getCategory() {
+        return category;
     }
 }
